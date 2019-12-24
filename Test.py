e409e305ee8d3798bd4049189c3941f1d810713d
gitwizs <z.amanuell@gmail.com>
2019-12-24 07:04:54 +0000

    Test.py

---
+++ Test.py
@@ -0,0 +1,19 @@
+import cv2
+import tensorflow as tf
+import numpy as np
+import os
+from PIL import Image
+import pandas as pd
+import matplotlib as mp
+import scipy as sc
+
+print(cv2.__version__)
+print(tf.__version__)
+print(np.__version__)
+print(os.path)
+print(Image.__version__)
+print(pd.__version__)
+print(mp.__version__)
+print(sc.__version__)
+
+print("Hello World. This is Task 2.")
